caf06e41ff9825b8ee2b13f85032155246afd2e2
Marcel Rudolf <marcel@digiorama.de>
2018-08-23 22:30:55 +0200

    Move from ReindeerWeb/ClassHelper

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# ClassHelper
+# ClassHelper by Novusvetus
 Some class helper.
+
+## Installation
+### Add it to your project with:
+```shell
+composer require novusvetus/classhelper
+```
+
+## License
+3-clause BSD license
+See [License](LICENSE)
+
+## Bugtracker
+Bugs are tracked in the issues section of this repository on GitHub.
+Please read over existing issues before submitting an issue to ensure yours is unique.
+
+[Create a new issue](../../issues/new)
+ - Describe the steps to reproduce your issue.
+ - Describe the expected and the actual outcome.
+ - Describe your environment as detailed as possible.
+
+## Development and contribution
+Feature requests can also be made by [creating a new issue](../../issues/new).
+If you would like to make contributions to this module, feel free to [create a fork](../../fork) and submit a pull request.
+
+## Versioning
+This project follows [Semantic Versioning](http://semver.org) paradigm. That is:
+
+> Given a version number MAJOR.MINOR.PATCH, increment the:
+>  1. MAJOR version when you make incompatible API changes,
+>  2. MINOR version when you add functionality in a backwards-compatible manner, and
+>  3. PATCH version when you make backwards-compatible bug fixes.
+> Additional labels for pre-release and build metadata are available as extensions to the MAJOR.MINOR.PATCH format.
---
+++ src/ClassHelper.php
@@ -0,0 +1,210 @@
+<?php
+
+/*
+ * This file is part of ClassHelper.
+ *
+ * (c) Novusvetus / Marcel Rudolf, Germany <development@novusvetus.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Novusvetus\ClassHelper;
+
+use ReflectionClass;
+
+/**
+ * This adds some helper for classes
+ */
+class ClassHelper
+{
+    private static $overwriteClasses = array();
+    private static $singletons = array();
+
+    /**
+     * The factory method, allows you to create an instance of a class.
+     *
+     * This can be called in two ways - calling via the class directly,
+     * or calling on Object and passing the class name as the first parameter.
+     *
+     * @param string $classOrArgument The first argument, or class name (if
+     * called directly on Object).
+     * @param mixed $argument,... arguments to pass to the constructor
+     *
+     * @return static
+     */
+    public static function create()
+    {
+        $args = func_get_args();
+
+        // Class to create should be the calling class if not Object,
+        // otherwise the first parameter
+        $class = get_called_class();
+        if ($class == 'Novusvetus\\ClassHelper\\ClassHelper') {
+            $class = array_shift($args);
+        }
+
+        $class = self::getOverwriteClass($class);
+
+        $r = new ReflectionClass($class);
+        return $r->newInstanceArgs($args);
+    }
+
+    /**
+     * Creates a class instance by the "singleton" design pattern.
+     * It will always return the same instance for this class.
+     *
+     * @param string $class Optional classname to create, if the called class should not be used
+     *
+     * @return static The singleton instance
+     */
+    public static function singleton($class = null)
+    {
+        if (!$class) {
+            $class = get_called_class();
+        }
+
+        if (!isset(self::$singletons[$class])) {
+            self::$singletons[$class] = self::create($class);
+        }
+
+        return self::$singletons[$class];
+    }
+
+    /**
+     * Returns true if a class or interface name exists.
+     *
+     * @param string $class
+     *
+     * @return bool
+     */
+    public static function exists($class)
+    {
+        return (class_exists($class, false) || interface_exists($class, false));
+    }
+
+    /**
+     * This class allows you to overload classes with other classes when they
+     * are constructed using the factory method
+     * {@link ClassHelper::create()}
+     *
+     * @param string $oldClass the class to replace
+     * @param string $newClass the class to replace it with
+     * @param bool $force When true, the new class don't need to be a child
+     * class of the old
+     *
+     * @return bool Returns true, if everything was okay
+     */
+    public static function useOverwriteClass($oldClass, $newClass, $force = false)
+    {
+        if (self::exists($newClass)) {
+            if (($force) || (is_a(self::singleton($oldClass), $newClass))) {
+                self::$overwriteClasses[$oldClass] = $newClass;
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * If a class has been overloaded, get the class name it has been
+     * overloaded with - otherwise return the class name
+     *
+     * @param string $class the class to check
+     *
+     * @return string the class that would be created if you called
+     * {@link ClassHelper::create()} with the class
+     */
+    public static function getOverwriteClass($class)
+    {
+        if (isset(self::$overwriteClasses[$class])) {
+            return self::$overwriteClasses[$class];
+        }
+
+        return $class;
+    }
+
+    /**
+     * Get the value of a static property of a class, even in that property
+     * is declared protected (but not private),
+     * without any inheritance, merging or parent lookup if it doesn't exist
+     * on the given class.
+     *
+     * @param string $class The class to get the static from
+     * @param string $name The property to get from the class
+     * @param mixed $default The value to return if property doesn't exist on
+     * class
+     *
+     * @return mixed The value of the static property $name on class $class,
+     * or $default if that property is not defined
+     */
+    public static function static_lookup($class, $name, $default = null)
+    {
+        $reflection = new ReflectionClass($class);
+        $static_properties = $reflection->getStaticProperties();
+
+        if (isset($static_properties[$name])) {
+            $value = $static_properties[$name];
+
+            $parent = get_parent_class($class);
+            if (!$parent) {
+                return $value;
+            }
+
+            $reflection = new ReflectionClass($parent);
+            $static_properties = $reflection->getStaticProperties();
+
+            if (!isset($static_properties[$name]) || $static_properties[$name] !== $value) {
+                return $value;
+            }
+        }
+
+        return $default;
+    }
+
+    /**
+     * Returns the parent class
+     *
+     * @return string this classes parent class
+     */
+    public function parentClass()
+    {
+        return get_parent_class($this);
+    }
+
+    /**
+     * Check if this class is an instance of a specific class, or has that
+     * class as one of its parents
+     *
+     * @param string $class
+     *
+     * @return bool
+     */
+    public function is_a($class)
+    {
+        return $this instanceof $class;
+    }
+
+    /**
+     * The class name
+     *
+     * @return string the class name
+     */
+    public function __toString()
+    {
+        return $this->class;
+    }
+
+    /**
+     * The class name
+     *
+     * @return string the class name
+     */
+    public function getClass()
+    {
+        return get_class($this);
+    }
+}
